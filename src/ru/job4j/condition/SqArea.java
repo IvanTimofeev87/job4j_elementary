733dbec8d8869c8a3afcc719e6ff650e59e127f6
IvanTimofeev87 <ivant87@mail.ru>
2020-12-20 23:27:51 +0300

    Add class ru.job4j.condition.ThreeMax and test for him.

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,9 +2,13 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        double height = p / (2 * (k + 1));
-        double rsl = (height * k) * height;
-        return rsl;
+        /*double height = p / (2 * (k + 1));
+        double rsl = (height + k) * height;
+        return rsl;*/
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 
     public static void main(String[] args) {
