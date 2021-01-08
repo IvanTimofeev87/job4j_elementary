9e0c785fb8cfbba69a319ad998459493b7d937e5
IvanTimofeev87 <ivant87@mail.ru>
2021-01-09 01:58:26 +0300

    Created class ru.job4j.loop.Mortgage and test for him.

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year (double amount, double salary, double percent) {
+        int year = 0;
+
+        while (amount > 1) {
+            double startCredSum= amount + amount * (percent/100);
+            amount = startCredSum - salary;
+            year += 1;
+        }
+        return year;
+    }
+}
