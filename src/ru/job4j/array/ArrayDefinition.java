33a0a4198874f91faea7155ce640edd5a9dbeec5
IvanTimofeev87 <ivant87@mail.ru>
2021-01-18 17:09:12 +0300

    Added to class ru.job4j.array.ArrayDefinition array "names"

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -3,10 +3,18 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        System.out.println("Размер массива равен: " + ages.length);
+        //System.out.println("Размер массива равен: " + ages.length);
         String[] surnames = new String[100500];
-        System.out.println("Размер массива равен: " + surnames.length);
+        //System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + prices.length);
+        //System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Peter Petrov";
+        names[2] = "Oleg Tinkoff";
+        names[3] = "Alex Smirnov";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
