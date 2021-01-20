f7455f1130342b7ade1f65bb3f813c064ca6aeab
IvanTimofeev87 <ivant87@mail.ru>
2021-01-20 23:34:43 +0300

    Created  class ru.job4j.array.Turn and test for him.

---
+++ src/ru/job4j/array/Turn.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        int temp1 = array[0];
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp1;
+        if (array.length % 2 != 0) {
+            for (int index = 1; index < array.length - 1; index++) {
+                if (index == array.length - index - 1) {
+                    break;
+                } else {
+                    int temp2 = array[index];
+                    array[index] = array[array.length - index - 1];
+                    array[array.length - index - 1] = temp2;
+                }
+            }
+        } else {
+            for (int index = 1; index < array.length - 1; index++) {
+                if (index > array.length - index - 1) {
+                    break;
+                } else {
+                    int temp2 = array[index];
+                    array[index] = array[array.length - index - 1];
+                    array[array.length - index - 1] = temp2;
+                }
+            }
+        }
+
+        return array;
+    }
+}
