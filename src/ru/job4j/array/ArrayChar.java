cec8708cdd15ce8c532639a7cfd4e74718bc9f2b
IvanTimofeev87 <ivant87@mail.ru>
2021-01-24 11:17:53 +0300

    Modified method startsWith in class ru.job4j.array.ArrayChar

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -5,14 +5,11 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int index = 0; index < pref.length; index++) {
-            if (word[index] == pref[index]) {
-                result = true;
-            } else {
+            if (word[index] != pref[index]) {
                 result = false;
                 break;
             }
         }
         return result;
     }
-
 }
