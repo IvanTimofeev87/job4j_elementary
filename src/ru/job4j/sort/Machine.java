e2467c62677bb4ec421cfca8687ac85cd17036f8
IvanTimofeev87 <ivant87@mail.ru>
2021-03-08 22:43:15 +0300

    Created class ru.job4j.array.Machine and test for him.

---
+++ src/ru/job4j/sort/Machine.java
@@ -0,0 +1,22 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int[] rsl = new int[100];
+        int size = 0;
+        int totalChange = money - price;
+
+        for (int i = 0; i < coins.length; i++) {
+            while (totalChange - coins[i] >= 0) {
+                rsl[size] = coins[i];
+                size += 1;
+                totalChange = totalChange - coins[i];
+            }
+        }
+
+        return Arrays.copyOf(rsl, size);
+    }
+}
